73ae20776ad19fa7b63b8d7a601db20c0d0573ab
Brent Weston Robinett <bwrobinett@gmail.com>
2022-08-25 22:51:32 -0700

    Use deployed model!

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 {
-  console.log("javascript code!");
-  document.addEventListener("load", () => {
-    document.body.innerHTML = "Goodbye";
+  /**
+   *
+   */
+  async function loaded(reader) {
+    const response = await fetch(
+      "https://hf.space/embed/hblings/fastai/+/api/predict",
+      {
+        method: "POST",
+        body: JSON.stringify({ data: [reader.result] }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const json = await response.json();
+    const label = json["data"][0]["confidences"][0]["label"];
+    const div = document.createElement("div");
+    div.innerHTML = `<br /><img src="${reader.result}" width="300px" /> <p>${label}</p>`;
+    document.body.append(div);
+  }
+
+  /**
+   *
+   */
+  function read(file) {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => loaded(reader));
+    reader.readAsDataURL(file);
+  }
+
+  window.addEventListener("load", () => {
+    document.getElementById("photos").addEventListener("input", () => {
+      [...photos.files].map(read);
+    });
   });
 }
